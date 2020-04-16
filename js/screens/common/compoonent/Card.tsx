23452bccf7c7afea2cf52a956c0e47cd3315ba48
soumyamishra89 <sm@aazzur.com>
2020-04-16 17:58:08 +0200

    updated card component card component will now display a footer note

--- js/screens/common/compoonent/Card.tsx
+++ js/screens/common/compoonent/Card.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Card as CardPaper, Title } from 'react-native-paper';
+import { Card as CardPaper, Title, Caption, Paragraph } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
 
 interface Props {
     action: () => void
     title: string,
-    description: string
+    description: string,
+    footerText?: string
 }
 
 export default function Card(props: Props) {
     return (
     <CardPaper elevation={4} onPress={props.action} style={styles.card}>
-        <CardPaper.Title title={props.title} titleStyle={styles.cardTitle}/>
-        <CardPaper.Content>
-            <Title>{props.description}</Title>        
+        <CardPaper.Content style={styles.content}>
+            <Title style={styles.cardTitle}>{props.title}</Title>
+        
+            <Paragraph>{props.description}</Paragraph>        
         </CardPaper.Content>
+        {props.footerText && <CardPaper.Actions style={styles.footer}>
+            <Caption>{props.footerText}</Caption>
+        </CardPaper.Actions>}
     </CardPaper>
   )
 }
@@ -23,9 +28,15 @@ const styles = StyleSheet.create({
     card: {
         marginHorizontal: 16,
         marginVertical: 8,
-        height: 168
+        minHeight: 144
     },
     cardTitle: {
         fontWeight: 'bold'
+    },
+    content: {
+        flex: 1
+    },
+    footer: {
+        marginLeft: 8
     }
 })
