4a3742f1d6de71143fcfefe90ea3c9741440d70a
soumyamishra89 <sm@aazzur.com>
2020-04-17 00:46:35 +0200

    added FAB to goto AddNewAuestion Screen

--- js/screens/questionlist/components/QuestionListComponent.tsx
+++ js/screens/questionlist/components/QuestionListComponent.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useCallback } from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
+import { FAB } from 'react-native-paper';
 import { useSafeArea } from 'react-native-safe-area-context';
 import Styles from '../../../styles';
 import Appbar from '../../../screens/common/compoonent/Appbar';
 import { PollQuestion } from '../../../data/types/PollsData';
 import { PollQuestionsProps, NavigationProps } from '../../../data/types/ScreenProps';
 import Card from '../../../screens/common/compoonent/Card';
+import colors from '../../../styles/colors';
 
 interface Props extends PollQuestionsProps, NavigationProps {}
 
@@ -13,7 +15,8 @@ export default function QuestionListComponent(props: Props) {
 
     const [searchedQuestions, setSearchedQuestion] = useState<PollQuestion[] | undefined>();
 
-    const goToQuestionDetailsScreen = (questionUrl: string) => props.navigation.navigate("QuestionDetailsScreen", { questionUrl })
+    const goToQuestionDetailsScreen = (questionUrl: string) => props.navigation.navigate("QuestionDetailsScreen", { questionUrl });
+    const goToAddQuestionScreen = () => props.navigation.navigate("AddNewQuestionScreen");
 
     const renderFlatListItem = useCallback(({ item }: {item: PollQuestion}): any => {
         return <Card title={item.question} action={() => goToQuestionDetailsScreen(item.url)} description={item.published_at.split('T')[0]} footerText={item.totalVotes.toString() + " votes"}/>;
@@ -31,6 +34,11 @@ export default function QuestionListComponent(props: Props) {
                 style={styles.container}
                 showsVerticalScrollIndicator={false}
             />
+             <FAB
+                style={[styles.fab, {bottom: inset.bottom}]}
+                icon="plus"
+                onPress={goToAddQuestionScreen}
+            />
         </View>
     )
 }
@@ -40,5 +48,12 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         backgroundColor: '#eeeeee'
+    },
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+        backgroundColor: colors.primary
     }
 })
