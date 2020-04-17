eb37faef6967921321a1484300a7861c8569aa48
soumyamishra89 <sm@aazzur.com>
2020-04-17 11:04:04 +0200

    made poll question as an optional. the redux store will have undefined  for pollquestions to show no questions after fetch call

--- js/services/redux/reducer/reducers.ts
+++ js/services/redux/reducer/reducers.ts
@@ -1,16 +1,16 @@
 import { PollQuestion, PollChoice } from "../../../data/types/PollsData";
 import { ReduxAction, ReduxActionType } from "../../../data/types/Redux";
 
-function pollQuestionsReducer(state: PollQuestion[] = [], action: ReduxAction) {
+function pollQuestionsReducer(state: PollQuestion[] | undefined, action: ReduxAction) {
     switch (action.type) {
         case ReduxActionType.POLL_QUESTION:
-            let pollQuestions = action.data;
-            if (pollQuestions) {
-                pollQuestions = pollQuestions.map((pollQuestion: any) => ({
-                    ...pollQuestion,
-                    totalVotes: Array.isArray(pollQuestion.choices) ? pollQuestion.choices.reduce((totalVotes: number, choice: PollChoice) => totalVotes + choice.votes, 0) : 0
-                }))
-            }
+            let pollQuestions = action.data || [];
+           
+            pollQuestions = pollQuestions.map((pollQuestion: any) => ({
+                ...pollQuestion,
+                totalVotes: Array.isArray(pollQuestion.choices) ? pollQuestion.choices.reduce((totalVotes: number, choice: PollChoice) => totalVotes + choice.votes, 0) : 0
+            }))
+        
             return pollQuestions;
         default: return state;
     }
